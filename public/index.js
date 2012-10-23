14da43b5773b1eb64b2baf0b721e2d30da0f73f2
Jacky Lee <JackyLeeHongJian@Gmail.com>
2012-10-23 11:03:59 +0800

    Removed ribbonCartoonize

--- public/index.js
+++ public/index.js
@@ -2,7 +2,6 @@ $(function() {
 	$.get('2ZD1.pdb', function(complex) {
 		var tc3d = new ChemDoodle.Canvas3D('TransformCanvas3D');
 		tc3d.specs.set3DRepresentation('Stick');
-		tc3d.specs.proteins_ribbonCartoonize = true;
 		tc3d.loadMolecule(ChemDoodle.readPDB(complex, 1));
 //		window.open(document.getElementById('TransformCanvas3D').toDataURL('image/png'));
 	});
