9b345539d01d737a14aeed40e466bd833dc2b095
Jacky Lee <JackyLeeHongJian@Gmail.com>
2012-10-23 11:13:33 +0800

    Renamed canvas id

--- public/index.js
+++ public/index.js
@@ -1,8 +1,8 @@
 $(function() {
 	$.get('2ZD1.pdb', function(complex) {
-		var tc3d = new ChemDoodle.Canvas3D('TransformCanvas3D');
-		tc3d.specs.set3DRepresentation('Stick');
-		tc3d.loadMolecule(ChemDoodle.readPDB(complex, 1));
+		var c = new ChemDoodle.Canvas3D('iview');
+		c.specs.set3DRepresentation('Stick');
+		c.loadMolecule(ChemDoodle.readPDB(complex, 1));
 //		window.open(document.getElementById('TransformCanvas3D').toDataURL('image/png'));
 	});
 });
