817848d0c5c00d73618ef1602660826cefb2adbc
Jacky Lee <JackyLeeHongJian@Gmail.com>
2012-10-24 22:49:03 +0800

    Simplified PDBInterpreter

--- public/ChemDoodleWeb.js
+++ public/ChemDoodleWeb.js
@@ -42,7 +42,6 @@ var ChemDoodle = (function() {
 	var c = {};
 
 	c.structures = {};
-	c.io = {};
 
 	return c;
 	
@@ -2064,188 +2063,53 @@ Line
 //  $LastChangedDate: 2012-05-05 15:12:11 -0400 (Sat, 05 May 2012) $
 //
 
-(function(c, extensions, io, structures, ELEMENT) {
+(function(c, extensions, structures, ELEMENT) {
 
-	io.PDBInterpreter = function() {
-
-		function checkContained(residue, set, chainID, index, helix) {
-			for ( var j = 0, jj = set.length; j < jj; j++) {
-				var check = set[j];
-				if (check.id == chainID && index >= check.start && index <= check.end) {
-					if (helix) {
-						residue.helix = true;
-					} else {
-						residue.sheet = true;
+	c.readPDB = function(content) {
+		var molecule = new structures.Molecule();
+		var resatoms = [];
+		var atomSerials = [];
+		var lines = content.split('\n');
+		for ( var i = 0, ii = lines.length; i < ii; i++) {
+			var line = lines[i];
+			if (extensions.stringStartsWith(line, 'ATOM')) {
+				var a = new structures.Atom($.trim(line.substring(76, 78)), parseFloat(line.substring(30, 38)), parseFloat(line.substring(38, 46)), parseFloat(line.substring(46, 54)));
+				a.hetatm = false;
+				a.resSeq = parseInt(line.substring(22, 26));
+				a.resName = $.trim(line.substring(17, 20));
+				resatoms.push(a);
+			} else if (extensions.stringStartsWith(line, 'HETATM')) {
+				var symbol = $.trim(line.substring(76, 78));
+				if (symbol.length > 1) {
+					symbol = symbol.substring(0, 1) + symbol.substring(1).toLowerCase();
+				}
+				var het = new structures.Atom(symbol, parseFloat(line.substring(30, 38)), parseFloat(line.substring(38, 46)), parseFloat(line.substring(46, 54)));
+				het.hetatm = true;
+				var residueName = $.trim(line.substring(17, 20));
+				molecule.atoms.push(het);
+				atomSerials[parseInt($.trim(line.substring(6, 11)))] = het;
+			} else if (extensions.stringStartsWith(line, 'TER')) {
+				// start a new chain.
+			} else if (extensions.stringStartsWith(line, 'ENDMDL')) {
+				break;
+			}
+		}
+		if(molecule.bonds.length==0){
+			for ( var i = 0, ii = molecule.atoms.length; i < ii; i++) {
+				for ( var j = i + 1; j < ii; j++) {
+					var first = molecule.atoms[i];
+					var second = molecule.atoms[j];
+					if (first.distance3D(second) < (ELEMENT[first.label].covalentRadius + ELEMENT[second.label].covalentRadius) * 1.1) {
+						molecule.bonds.push(new structures.Bond(first, second, 1));
 					}
-					if (index + 1 == check.end) {
-						residue.arrow = true;
-					}
-					return;
 				}
 			}
 		}
-
-		this.read = function(content) {
-			var molecule = new structures.Molecule();
-			molecule.chains = [];
-			var currentTagTokens = content.split('\n');
-			var helices = [];
-			var sheets = [];
-			var lastC = null;
-			var currentChain = [];
-			var resatoms = [];
-			var atomSerials = [];
-			for ( var i = 0, ii = currentTagTokens.length; i < ii; i++) {
-				var line = currentTagTokens[i];
-				if (extensions.stringStartsWith(line, 'HELIX')) {
-					helices.push({
-						id : line.substring(19, 20),
-						start : parseInt(line.substring(21, 25)),
-						end : parseInt(line.substring(33, 37))
-					});
-				} else if (extensions.stringStartsWith(line, 'SHEET')) {
-					sheets.push({
-						id : line.substring(21, 22),
-						start : parseInt(line.substring(22, 26)),
-						end : parseInt(line.substring(33, 37))
-					});
-				} else if (extensions.stringStartsWith(line, 'ATOM')) {
-					var label = $.trim(line.substring(76, 78));
-					var a = new structures.Atom(label, parseFloat(line.substring(30, 38)), parseFloat(line.substring(38, 46)), parseFloat(line.substring(46, 54)));
-					a.hetatm = false;
-					resatoms.push(a);
-					// set up residue
-					var resSeq = parseInt(line.substring(22, 26));
-					if (currentChain.length == 0) {
-						for ( var j = 0; j < 2; j++) {
-							var dummyFront = new structures.Residue(-1);
-							dummyFront.cp1 = a;
-							dummyFront.cp2 = a;
-							currentChain.push(dummyFront);
-						}
-					}
-					if (resSeq != Number.NaN && currentChain[currentChain.length - 1].resSeq != resSeq) {
-						var r = new structures.Residue(resSeq);
-						r.name = $.trim(line.substring(17, 20));
-						r.name = r.name.substring(0, 1) + r.name.substring(1).toLowerCase();
-						currentChain.push(r);
-						var chainID = line.substring(21, 22);
-						checkContained(r, helices, chainID, resSeq, true);
-						checkContained(r, sheets, chainID, resSeq, false);
-					}
-					// end residue setup
-					var atomName = $.trim(line.substring(12, 16));
-					var currentResidue = currentChain[currentChain.length - 1];
-					if (atomName == 'CA' || atomName == 'P' || atomName == 'O5\'') {
-						if (!currentResidue.cp1) {
-							currentResidue.cp1 = a;
-						}
-					} else if (atomName == 'N3' && (currentResidue.name=='C'||currentResidue.name=='U'||currentResidue.name=='T') || atomName == 'N1' && (currentResidue.name=='A'||currentResidue.name=='G')) {
-						//control points for base platform direction
-						currentResidue.cp3 = a;
-					} else if (atomName == 'C2') {
-						//control points for base platform orientation
-						currentResidue.cp4 = a;
-					} else if (atomName == 'C4' && (currentResidue.name=='C'||currentResidue.name=='U'||currentResidue.name=='T') || atomName == 'C6' && (currentResidue.name=='A'||currentResidue.name=='G')) {
-						//control points for base platform orientation
-						currentResidue.cp5 = a;
-					} else if (atomName == 'O' || atomName == 'C6' && (currentResidue.name=='C'||currentResidue.name=='U'||currentResidue.name=='T') || atomName == 'N9') {
-						if (!currentChain[currentChain.length - 1].cp2) {
-							if (atomName == 'C6' || atomName == 'N9') {
-								lastC = a;
-							}
-							currentResidue.cp2 = a;
-						}
-					} else if (atomName == 'C') {
-						lastC = a;
-					}
-				} else if (extensions.stringStartsWith(line, 'HETATM')) {
-					var symbol = $.trim(line.substring(76, 78));
-					if (symbol.length > 1) {
-						symbol = symbol.substring(0, 1) + symbol.substring(1).toLowerCase();
-					}
-					var het = new structures.Atom(symbol, parseFloat(line.substring(30, 38)), parseFloat(line.substring(38, 46)), parseFloat(line.substring(46, 54)));
-					het.hetatm = true;
-					var residueName = $.trim(line.substring(17, 20));
-					if (residueName == 'HOH') {
-						het.isWater = true;
-					}
-					molecule.atoms.push(het);
-					atomSerials[parseInt($.trim(line.substring(6, 11)))] = het;
-				} else if(extensions.stringStartsWith(line, 'CONECT')){
-					var oid = parseInt($.trim(line.substring(6, 11)));
-					if(atomSerials[oid]){
-						var origin = atomSerials[oid];
-						for(var k = 0; k<4; k++){
-							var next = $.trim(line.substring(11+k*5, 16+k*5));
-							if(next.length!=0){
-								var nid = parseInt(next);
-								if(atomSerials[nid]){
-									var a2 = atomSerials[nid];
-									var found = false;
-									for(var j = 0, jj = molecule.bonds.length; j<jj; j++){
-										var b = molecule.bonds[j];
-										if(b.a1==origin&&b.a2==a2||b.a1==a2&&b.a2==origin){
-											found = true;
-											break;
-										}
-									}
-									if(!found){
-										molecule.bonds.push(new structures.Bond(origin, a2));
-									}
-								}
-							}
-						}
-					}
-				} else if (extensions.stringStartsWith(line, 'TER')) {
-					this.endChain(molecule, currentChain, lastC);
-					currentChain = [];
-				} else if (extensions.stringStartsWith(line, 'ENDMDL')) {
-					break;
-				}
-			}
-			this.endChain(molecule, currentChain, lastC);
-			if(molecule.bonds.length==0){
-				for ( var i = 0, ii = molecule.atoms.length; i < ii; i++) {
-					for ( var j = i + 1; j < ii; j++) {
-						var first = molecule.atoms[i];
-						var second = molecule.atoms[j];
-						if (first.distance3D(second) < (ELEMENT[first.label].covalentRadius + ELEMENT[second.label].covalentRadius) * 1.1) {
-							molecule.bonds.push(new structures.Bond(first, second, 1));
-						}
-					}
-				}
-			}
-			molecule.atoms = molecule.atoms.concat(resatoms);
-			return molecule;
-		};
-		this.endChain = function(molecule, chain, lastC) {
-			if (chain.length > 0) {
-				var last = chain[chain.length - 1];
-				if (!last.cp1) {
-					last.cp1 = molecule.atoms[molecule.atoms.length - 2];
-				}
-				if (!last.cp2) {
-					last.cp2 = molecule.atoms[molecule.atoms.length - 1];
-				}
-				for ( var i = 0; i < 4; i++) {
-					var dummyEnd = new structures.Residue(-1);
-					dummyEnd.cp1 = lastC;
-					dummyEnd.cp2 = chain[chain.length - 1].cp2;
-					chain.push(dummyEnd);
-				}
-				molecule.chains.push(chain);
-			}
-		};
+		molecule.atoms = molecule.atoms.concat(resatoms);
+		return molecule;
 	};
 
-	// shortcuts
-	var interpreter = new io.PDBInterpreter();
-	c.readPDB = function(content) {
-		return interpreter.read(content);
-	};
-
-})(ChemDoodle, ChemDoodle.extensions, ChemDoodle.io, ChemDoodle.structures, ChemDoodle.ELEMENT);
+})(ChemDoodle, ChemDoodle.extensions, ChemDoodle.structures, ChemDoodle.ELEMENT);
 
 //
 //  Copyright 2009 iChemLabs, LLC.  All rights reserved.
