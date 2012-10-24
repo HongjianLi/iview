05323abbfe29bbccca93f0c4eb46dadc7985853f
Jacky Lee <JackyLeeHongJian@Gmail.com>
2012-10-24 22:56:17 +0800

    Removed math.angleBetweenLargest and math.hsl2rgb

--- public/ChemDoodleWeb.js
+++ public/ChemDoodleWeb.js
@@ -123,45 +123,6 @@ ChemDoodle.math = (function(extensions, structures) {
 
 	var pack = {};
 
-	pack.angleBetweenLargest = function(angles) {
-		if (angles.length == 0) {
-			return {
-				angle : 0,
-				largest : Math.PI * 2
-			};
-		}
-		if (angles.length == 1) {
-			return {
-				angle : angles[0] + Math.PI,
-				largest : Math.PI * 2
-			};
-		}
-		var largest = 0;
-		var angle = 0;
-		var index = -1;
-		for ( var i = 0, ii = angles.length - 1; i < ii; i++) {
-			var dif = angles[i + 1] - angles[i];
-			if (dif > largest) {
-				largest = dif;
-				angle = (angles[i + 1] + angles[i]) / 2;
-				index = i;
-			}
-		}
-		var last = angles[0] + Math.PI * 2 - angles[angles.length - 1];
-		if (last > largest) {
-			angle = angles[0] - last / 2;
-			largest = last;
-			if (angle < 0) {
-				angle += Math.PI * 2;
-			}
-			index = angles.length - 1;
-		}
-		return {
-			angle : angle,
-			largest : largest
-		};
-	};
-
 	pack.isBetween = function(x, left, right) {
 		return x >= left && x <= right;
 	};
@@ -259,33 +220,6 @@ ChemDoodle.math = (function(extensions, structures) {
 			};
 		else
 			return false;
-	};
-
-	pack.hsl2rgb = function(h, s, l) {
-		var r, g, b;
-		if (s == 0) {
-			r = g = b = l; // achromatic
-		} else {
-			function hue2rgb(p, q, t) {
-				if (t < 0)
-					t += 1;
-				if (t > 1)
-					t -= 1;
-				if (t < 1 / 6)
-					return p + (q - p) * 6 * t;
-				if (t < 1 / 2)
-					return q;
-				if (t < 2 / 3)
-					return p + (q - p) * (2 / 3 - t) * 6;
-				return p;
-			}
-			var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-			var p = 2 * l - q;
-			r = hue2rgb(p, q, h + 1 / 3);
-			g = hue2rgb(p, q, h);
-			b = hue2rgb(p, q, h - 1 / 3);
-		}
-		return [ r * 255, g * 255, b * 255 ];
 	};
 
 	return pack;
