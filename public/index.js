1f33220d35f42f082ea96bfe0d6ef487f67bf392
Jacky Lee <JackyLeeHongJian@Gmail.com>
2012-11-04 09:02:58 +0800

    Used ligand.pdbqt

--- public/index.js
+++ public/index.js
@@ -8,7 +8,7 @@ $(function() {
 	$.get('receptor.pdbqt', function(receptor) {
 		c.setBox([49.712, -28.923, 36.824], [18, 18, 20]);
 		c.parseReceptor(receptor);
-		$.get('ligand2.pdbqt', function(ligand) {
+		$.get('ligand.pdbqt', function(ligand) {
 			c.parseLigand(ligand);
 			c.repaint();
 		});
