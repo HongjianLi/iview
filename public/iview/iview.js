7814fddb10cdeba8a87310ce0869bc198e25ea03
Jacky Lee <JackyLeeHongJian@Gmail.com>
2013-02-26 11:31:52 +0800

    Added code to draw dashed lines for solvents. Bugs remain

--- public/iview/iview.js
+++ public/iview/iview.js
@@ -655,7 +655,15 @@ var iview = (function () {
 			for (var j in atom1.bonds) {
 				var atom2 = atoms[atom1.bonds[j]];
 				if (atom2.serial < atom1.serial) continue;
-				this.drawBondsAsLineSub(geo, atom1, atom2);
+				if (atom1.solvent) {
+					var geo2 = new THREE.Geometry();
+					geo2.vertices.push(new THREE.Vector3(atom1.x, atom1.y, atom1.z));
+					geo2.vertices.push(new THREE.Vector3(atom2.x, atom2.y, atom2.z));
+					geo2.computeLineDistances();
+					this.modelGroup.add(new THREE.Line(geo2, new THREE.LineDashedMaterial({ 'color': atom1.color, dashSize: 0.25, gapSize: 0.125 })));
+				} else {
+					this.drawBondsAsLineSub(geo, atom1, atom2);
+				}
 			}
 		}
 		this.modelGroup.add(new THREE.Line(geo, new THREE.LineBasicMaterial({ linewidth: lineWidth, vertexColors: true }), THREE.LinePieces));
@@ -894,12 +902,12 @@ var iview = (function () {
 		this.drawStrand(beta, undefined, undefined, true, 0, this.helixSheetWidth, false, this.thickness * 2);
 	};
 
-	iview.prototype.drawDashLines = function (p1, p2, color) {
+	iview.prototype.drawDashLines = function (atom1, atom2, color) {
 		var geo = new THREE.Geometry();
-		geo.vertices.push(p1);
-		geo.vertices.push(p2);
+		geo.vertices.push(new THREE.Vector3(atom1.x, atom1.y, atom1.z));
+		geo.vertices.push(new THREE.Vector3(atom2.x, atom2.y, atom2.z));
 		geo.computeLineDistances();
-		this.modelGroup.add(new THREE.Line(geo, new THREE.LineDashedMaterial({ 'color': color, dashSize: 0.5, gapSize: 0.25 })));
+		this.modelGroup.add(new THREE.Line(geo, new THREE.LineDashedMaterial({ 'color': atom1.color, dashSize: 0.5, gapSize: 0.25 })));
 	};
 
 	iview.prototype.colorByElement = function (atoms) {
